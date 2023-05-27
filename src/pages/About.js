60dbcfaae22872931a307611274d6a7836c181e0
unknown <danny.b222@hotmail.com>
2023-05-27 17:08:33 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        vero vel officiis nihil facere eum voluptate accusantium temporibus.
+        Laboriosam, cumque explicabo eligendi libero molestias id velit, hic
+        facilis deleniti architecto est tempora nemo delectus itaque, ducimus
+        labore modi non rem?
+      </p>
+    </section>
+  );
+};
+
+export default About;
